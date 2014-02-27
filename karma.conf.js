89a50ccf9dd41d6d47677f0bd242dff17255de48
Vojta Jina <vojta.jina@gmail.com>
2014-02-27 14:33:54 -0800

    Update to ng 1.2.13, use bower

--- karma.conf.js
+++ karma.conf.js
@@ -5,9 +5,11 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
     files: [
-      'angular.js',
-      'angular-resource.js',
-      'angular-mocks.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'todo.js',
       'todoSpec.js',
       'todoMongoSpec.js',
